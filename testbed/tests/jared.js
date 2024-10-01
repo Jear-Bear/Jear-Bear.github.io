c02fda562a82693d7b428a1cdb7d1a8b10aff06a
Jared Perlmutter <jperlmutter1@gmail.com>
2024-10-01 17:36:14 -0500

    Update jared.js

--- testbed/tests/jared.js
+++ testbed/tests/jared.js
@@ -14,7 +14,7 @@ youtube.src = "youtube.png";
 var instagram = new Image();
 instagram.src = "instagram.png";
 var text = new Image();
-text.src = "Text.png";
+text.src = "text.png";
 
 const rgbaColors = [
   [54, 74, 211],  
